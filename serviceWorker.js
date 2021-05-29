95afe3fd5c3ee3e8b56ad70bdf7080debc776004
ChrisJobs046 <ChrisJobs046@gmail.com>
2021-05-29 17:45:18 -0400

    Agregamos lo que seria nuestro archivo de cache

---
+++ serviceWorker.js
@@ -0,0 +1,25 @@
+const staticDevPWACoffee = "dev-pwacoffee-site-v1";
+const assets = [
+
+    "/",
+    "/Index.html",
+    "/Style.css",
+    "/App.js",
+    "/images/coffee1.jpg",
+    "/images/coffee2.jpg",
+    "/images/coffee3.jpg",
+    "/images/coffee4.jpg",
+    "/images/coffee5.jpg",
+    "/images/coffee6.jpg",
+    "/images/coffee7.jpg",
+    "/images/coffee8.jpg",
+    "/images/coffee9.jpg",
+];
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticDevPWACoffee).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
