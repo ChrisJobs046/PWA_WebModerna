03859e1e6312456761a24cb443d433b5cadf3014
ChrisJobs046 <ChrisJobs046@gmail.com>
2021-06-01 18:07:55 -0400

    Realizando Mejoras

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ const MostrarCoffee = () => {
             <div class="card">
                 <img class="card--avatar" src=${image} />
                 <h1 class="card--title">${name}</h1>
-                <a class="card--link" href='#'>Taste</a>
+                <button class="card--link" href='#'>Ver</button>
             </div>
             `)
     )
